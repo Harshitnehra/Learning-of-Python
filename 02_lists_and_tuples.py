d12d843c466aeacbcea1d69ffc16f5290dfb2d2a
Harshit nehra <nehraharshit01@gmail.com>
2024-09-20 14:15:04 +0530

    add string or list, tuples

---
+++ 02_lists_and_tuples.py
@@ -0,0 +1,68 @@
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+
+tropical.extend(thislist)
+
+# print(thislist)
+# print(tropical)
+
+word = "harshit*************nehra"
+
+# print(word.replace("*", " "))harsk
+
+
+a = "Hello, World!"
+b = a.split(" ")
+# print(b)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# num = thislist[1] = "blackcurrant"
+# print(thislist)
+# print(num)
+
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana","a"]
+
+thislist.sort()
+
+# print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry","a"]
+
+thislist.sort()
+
+# print(thislist)
+
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+# print(thislist)
+
+a  = ("df","3","bdfdsb","5",True)
+d = a[1:4]
+# print(type(a))
+# print(a)
+# print(d)
+fruits = []
+# name = int(input("enter name"))
+# fruits.append(name)
+# name1 = int(input("enter name"))
+# fruits.append(name1)
+# name2 = int(input("enter name"))
+# fruits.append(name2)
+# name3 = int(input("enter name"))
+# fruits.append(name3)
+# fruits.sort()
+# print(fruits)
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = thistuple.count(5)
+
+print(x)
+
+login = (1,2,2,2,2,3,45,6,7,8)
+
+
+print((login.count(2)))
