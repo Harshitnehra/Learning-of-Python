f0d6be0dea56457a2db34ef5f98b0179a109256b
Harshit nehra <nehraharshit01@gmail.com>
2024-09-20 14:16:37 +0530

    add dictionaries

---
+++ 03_Dictionaries.py
@@ -0,0 +1,60 @@
+marks = {
+    "harshit": 100,
+    "siddharth": 45,
+    "reenu": 78,
+    0: 90,
+    "list": [1,2,3,4]
+}
+
+# print(marks[0])
+# print(marks["harshit"] ,type(marks))
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+# print(thisdict[""])
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+# print(thisdict["brand"])
+# print(len(thisdict))
+
+# print(thisdict.get("fdgd"))
+# print(thisdict["brangggd"])
+a = thisdict.keys()
+# print(a)
+# print(thisdict.keys())
+
+
+
+car = {
+"brand": "Ford",
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+# print(x) #before the change
+
+car["color"] = "white"
+
+# print(x) #after the change
+# print(car)
+# print(car.values())
+
+thisdict = {
+  "brand": "Ford",
+  "brand": "cnghcnh",
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print()
