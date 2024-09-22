247a55dbedc3d0874e47977a0b24c11cbbff2c1b
Harshit nehra <nehraharshit01@gmail.com>
2024-09-22 10:53:34 +0530

    oops-intense

---
+++ 10_oops_intence.py
@@ -0,0 +1,8 @@
+class employee:
+    company = "google"
+    language = "python"
+    salery = "1200000"
+
+harshit = employee()
+harshit.company= "microsoft"
+print(harshit.company, harshit.language)
