2d09061f764a19bdb7cc01cb688e57fbe4bffa98
Harshit nehra <nehraharshit01@gmail.com>
2024-10-04 22:18:10 +0530

    merge

---
+++ 19_merge.py
@@ -0,0 +1,12 @@
+thisdict = {
+  "dfsd": "Ford",
+  "dsgsdv": "Mustang",
+  "dsgverb": 1964
+}
+thisdict1 = {
+  "brand": "harshit",
+  "model": "nehta",
+  "year": 2001
+}
+merged = thisdict | thisdict1
+print(merged)
