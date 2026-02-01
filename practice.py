07bc3fb8451ed83c6aad7444c878d570b4bded1f
Harshitnehra <158832953+Harshitnehra@users.noreply.github.com>
2026-02-01 13:26:24 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,13 @@
 import calendar
 
 print(calendar.month(12366576576547645,4))
+import calendar
 
+print(calendar.month(12366576576547645,4))
+
+# fjxhcbykuhsfbxv,jimport calendar
+
+print(calendar.month(12366576576547645,4))
+
+# fjxhcbykuhsfbxv,j
 # fjxhcbykuhsfbxv,j
