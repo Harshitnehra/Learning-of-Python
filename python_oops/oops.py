a4e2bad3b1495e2aa93cf9d015dfcb84183f6785
Harshit nehra <nehraharshit01@gmail.com>
2026-08-06 08:38:49 +0530

    Add System

--- python_oops/oops.py
+++ python_oops/oops.py
@@ -1,37 +1,53 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
 
-s1 = Student("Harshit")
+# s1 = Student("Harshit")
 
-print("Before:", s1.name)
+# print("Before:", s1.name)
 
-# Modify attribute
-s1.name = "Rahul"
+# # Modify attribute
+# s1.name = "Rahul"
 
-print("After:", s1.name)
-
-
-class Student:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return f"Student Name: {self.name}"
-
-s1 = Student("Harshit")
-
-print(s1)   # Calls __str__()
+# print("After:", s1.name)
 
 
-class Student:
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
 
-    count = 0
+#     def __str__(self):
+#         return f"Student Name: {self.name}"
 
-    def __init__(self):
-        Student.count += 1
+# s1 = Student("Harshit")
 
-s1 = Student()
-s2 = Student()
+# print(s1)   # Calls __str__()
 
-print(Student.count)
+
+# class Student:
+
+#     count = 0
+
+#     def __init__(self):
+#         Student.count += 1
+
+# s1 = Student()
+# s2 = Student()
+
+# print(Student.count)
+
+
+
+class A:
+    def show(self):
+        print("A")
+
+class B:
+    def show(self):
+        print("B")
+
+class C(B , A):
+    pass
+
+obj = C()
+obj.show()
