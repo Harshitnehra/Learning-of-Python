567febbd2f236da606a23968ecfb10b7435cd5e3
Harshit nehra <nehraharshit01@gmail.com>
2026-08-07 09:04:13 +0530

    Add System

---
+++ python_oops/list.py
@@ -0,0 +1,18 @@
+numbers = [10,20,30]
+
+empty = []
+
+list2 = list((1,2,3))
+
+print(list2)
+
+lists = [1,[1,2],2,3,45]
+
+lists.insert(1 , [1,2])
+print(lists)
+
+nums=[1,2,3]
+
+nums.remove(2)
+
+print(nums)
